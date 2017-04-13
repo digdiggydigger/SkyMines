c558b1b61fded16909d78ec44d4c5a512bdab9da
digdiggydigger <ethanant@icloud.com>
2017-04-13 23:15:53 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	bin/me/LavaBa11/SkyMines.class
    #	src/me/LavaBa11/PlayerJoin/PlayerJoin.java
    #	src/me/LavaBa11/SkyMines.java

--- src/me/LavaBa11/PlayerJoin/PlayerJoin.java
+++ src/me/LavaBa11/PlayerJoin/PlayerJoin.java
@@ -1,5 +1,37 @@
 package me.LavaBa11.PlayerJoin;
 
+<<<<<<< Updated upstream
 public class PlayerJoin {
+=======
+<<<<<<< HEAD
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import me.LavaBa11.SkyMines;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle.EnumTitleAction;
+
+public class PlayerJoin implements Listener {
+
+	public PlayerJoin(SkyMines plugin) {
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);	
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		//Main title
+		PacketPlayOutTitle title = new PacketPlayOutTitle(EnumTitleAction.TITLE, ChatSerializer.a("{\"text\":\"§c§lPsychic§7§lNetwork\"}"), 10, 1000, 10);
+		((CraftPlayer) e.getPlayer()).getHandle().playerConnection.sendPacket(title);
+		//Subtitle
+		PacketPlayOutTitle subtitle = new PacketPlayOutTitle(EnumTitleAction.SUBTITLE, ChatSerializer.a("{\"text\":\"§7§lWelcome!\"}"), 10, 1000, 10);
+		((CraftPlayer) e.getPlayer()).getHandle().playerConnection.sendPacket(subtitle);
+	}
+=======
+public class PlayerJoin {
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 	
 }
--- src/me/LavaBa11/SkyMines.java
+++ src/me/LavaBa11/SkyMines.java
@@ -6,19 +6,44 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.LavaBa11.Mines.MineListener;
+import me.LavaBa11.PlayerJoin.PlayerJoin;
 
 public class SkyMines extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+<<<<<<< Updated upstream
 		getLogger().info("_____ has been enabled and ready to go!");
 		new MineListener(this); 
+=======
+<<<<<<< HEAD
+			getLogger().info("SkyMines has been enabled and ready to go!");
+			new MineListener(this); {
+			new PlayerJoin(this);
+	
+			}
+=======
+		getLogger().info("_____ has been enabled and ready to go!");
+		new MineListener(this); 
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 	}
 			
 	@Override
 	public void onDisable() {
+<<<<<<< Updated upstream
 		getLogger().info("_____ has been disabled safley and securely");
 	}
+=======
+<<<<<<< HEAD
+			getLogger().info("SkyMines has been disabled safley and securley");
+
+}
+=======
+		getLogger().info("_____ has been disabled safley and securely");
+	}
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
