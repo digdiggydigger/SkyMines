22c54e2cca19f4a9309cccff22e71cf32c0280e5
digdiggydigger <ethanant@icloud.com>
2017-04-16 03:24:34 +0100

    Help Message

--- src/me/LavaBa11/Messages/Help.java
+++ src/me/LavaBa11/Messages/Help.java
@@ -17,7 +17,7 @@ public class Help implements Listener {
 		Player player = (Player) sender;
 		sender.sendMessage("§7§m---------=(§r §9§nSkyMines§7 )§7=§m---------");
 		sender.sendMessage("                                                 ");
-		sender.sendMessage("§7Welcome To SkyMines §6" + player.getDisplayName() + " §7!");
+		sender.sendMessage("§7Welcome To SkyMines §6" + player.getName() + " §7!");
 		sender.sendMessage("§7SkyMines is a custom made, original idea made in house!");
 		sender.sendMessage("§7SkyMines is a mix of the two popular gamemodes:");
 		sender.sendMessage("           §6§nPrison§7 and+ §6§nSkyBlock§7!");
